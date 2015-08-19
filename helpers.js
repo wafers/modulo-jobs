143bce5e7695eee96333adb16af0f833158444d0
quikstrike <quikstrik3@gmail.com>
2015-08-19 09:31:04 -0500

    Parsing Registry Database, and compare with local records.

--- helpers.js
+++ helpers.js
@@ -1,4 +1,23 @@
+var config = require(__dirname + '/config.js')
 var request = require('request');
+var _ = require('underscore')
+var mongo = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
+var url = process.env.MONGOLAB_URI || config.mongodb;
+var DB;
+var jobs
+
+MongoClient.connect(url, function(err, db) {
+    if(err) console.log(err)
+    console.log('Correctly connects to the database');
+    DB = db;
+    jobs = DB.collection('jobs');
+})
+
+
+
+var Timestamp = mongo.Timestamp;
+
 
 var getAllNamesFromRegistry = module.exports.getAllNamesFromRegistry = function(cb) {
   //Retrieve all module names from the registry,
@@ -16,5 +35,40 @@ var getAllNamesFromRegistry = module.exports.getAllNamesFromRegistry = function(
         })
         console.log(names.length, 'modules found');
         cb(names);
+    })
+}
+
+var databaseInit = module.exports.databaseInit = function() {
+  //Retrieve all module names from the registry,
+  // and run a callback function on them
+  getAllNamesFromRegistry(function(moduleArray){
+    // console.log('array',resultsArray);
+    // {module: 'angular', status: 'ready', lastUpdated: 'timestamp'}
+    getAllNamesFromDb(moduleArray,function(collection,items){
+        console.log("Getting Diff")
+        console.log(_.difference(collection,items))
+    })
+
+    // for (var i = 0; i < resultsArray.length; i++){
+    //     var ts = new Timestamp();
+    //     var current = {module: resultsArray[i], status: 'ready', lastUpdated: ts};
+    //     //add data to current
+    //     DB.collection('jobs').insert(current)
+    // }
+  })
+}
+
+var getAllNamesFromDb = module.exports.getAllNamesFromDb = function(moduleArray,cb) {
+
+    // jobs.find().toArray(function(err, docs) {
+    //     var cleanArray = []
+    //     docs.forEach(function(item){
+    //         cleanArray.push(item.module)
+    //     })
+
+    //     // cb(moduleArray,cleanArray)
+    // });
+    jobs.distinct('module',function(err,docs){
+        console.log(docs)
     })
 }
